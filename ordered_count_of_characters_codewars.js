3192b7ef799bdfbaa52ee60db1c652be28e55466
Crew Spence <csspence123@gmail.com>
2020-07-27 07:13:36 -0500

    Wrote naive solution, but need to cover some edge cases.

--- ordered_count_of_characters_codewars.js
+++ ordered_count_of_characters_codewars.js
@@ -6,7 +6,21 @@ Example:
 orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
 */
 
-var orderedCount = function (text) {
-  // Implement me!
-  return [];
+const orderedCount = (text) => {
+  let obj = {};
+  let arr = [];
+  let final = [];
+  for(let i = 0; i < text.length; i++) {
+    if(obj[text[i]] === undefined) {
+      obj[text[i]] = 1;
+    } else {
+      obj[text[i]]++;
+    }
+  }
+  arr = Object.keys(obj);
+  for(let i = 0; i < arr.length; i++) {
+    final.push([arr[i], obj[arr[i]]]);
+  }
+
+  return final;
 }
